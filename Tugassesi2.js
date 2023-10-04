99a4857715d7c92c036ca614dd05e9871e6f7062
siti nurviatika <114561174+nurviatika@users.noreply.github.com>
2023-10-04 19:42:34 +0700

    Add files via upload

---
+++ Tugassesi2.js
@@ -0,0 +1,11 @@
+// identitas diri
+
+var firstName = "Siti"
+var lastName = "Nurviatika"
+var age = "19"
+var isMarried = "False"
+
+console.log(`firstName : ${firstName}`)
+console.log(`lastName : ${lastName}`)
+console.log(`age : ${age}`)
+console.log(`isMarried : ${isMarried}`)
